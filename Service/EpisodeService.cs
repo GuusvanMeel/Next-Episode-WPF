9c35f8d5628d09c83c5bcce96d19e5a30406466b
GuusvanMeel <vanmeelguus@gmail.com>
2025-06-23 02:11:34 +0200

    episodeservice almost done to do list made

--- Service/EpisodeService.cs
+++ Service/EpisodeService.cs
@@ -1,25 +1,18 @@
 ﻿using Interfaces.Entities;
 using Interfaces.interfaces;
-using System.Net.NetworkInformation;
-using System.Runtime.ConstrainedExecution;
 
 namespace Service
 {
-    public class EpisodeService
+    public class EpisodeService(IShowRepo Repo, ILoggerService _logger)
     {
-        private IShowRepo ShowRepo;
-        private ILoggerService logger;
-        static string noshow = "could not find show";
-        public EpisodeService(IShowRepo _repo, ILoggerService _logger)
-        {
-            this.ShowRepo = _repo;
-            this.logger = _logger;
-        }
+        private readonly IShowRepo ShowRepo = Repo;
+
+        private readonly ILoggerService logger = _logger;
+
         public ResponseBody LogEpisodeWatched(Show show)
         {
             try
             {
-
 
                 Episode? justwatched = FindCurrentEpisode(show);
                 if (justwatched == null)
@@ -39,10 +32,12 @@ namespace Service
             }
             catch (Exception ex)
             {
-                logger.LogException("LogEpisodeWatched", ex);
+                logger.LogException(nameof(FindCurrentEpisode), ex);
+
                 return ResponseBody.Fail("Unexpected error while logging episode.");
             }
         }
+
         public ResponseBody SetCurrentEpisode(Episode episode)
         {
             try
@@ -50,7 +45,7 @@ namespace Service
                 Show? show = ShowRepo.GetShow(episode.ShowName);
                 if (show == null)
                 {
-                    return ResponseBody.Fail(noshow);
+                    return ResponseBody.Fail("could not find show");
                 }
                 show.CurrentEpisodePath = episode.FilePath;
                 ShowRepo.SaveShow(show);
@@ -58,10 +53,12 @@ namespace Service
             }
             catch (Exception ex)
             {
-                logger.LogException("SetCurrentEpisode", ex);
+                logger.LogException(nameof(SetCurrentEpisode), ex);
+
                 return ResponseBody.Fail("Unexpected error while setting new current episode.");
             }
         }
+
         public ResponseBody<Episode> GetCurrentEpisode(Show show)
         {
             try
@@ -69,23 +66,27 @@ namespace Service
 
                 Episode? current = FindCurrentEpisode(show);
                 if (current == null)
+                {
                     return ResponseBody<Episode>.Fail("No current episode set or episode not found.");
+                }
 
                 return ResponseBody<Episode>.Ok(current);
             }
             catch (Exception ex)
             {
-                logger.LogException("GetCurrentEpisode", ex);
+                logger.LogException(nameof(GetCurrentEpisode), ex);
+
                 return ResponseBody<Episode>.Fail("Unexpected error while retrieving current episode.");
             }
         }
+
         public ResponseBody<Episode> GetNextEpisode(Show show)
         {
             try
             {
                 ResponseBody<Episode> currentResult = GetCurrentEpisode(show);
                 if (!currentResult.Success)
-                    return ResponseBody<Episode>.Fail(currentResult.Message!);
+                    return ResponseBody<Episode>.Fail(currentResult.Message ?? "Current Episode Error");
 
                 Episode? next = GetNextEpisode(show, currentResult.Data!);
                 if (next == null)
@@ -97,48 +98,89 @@ namespace Service
             }
             catch (Exception ex)
             {
-                logger.LogException("GetNextEpisode", ex);
+                logger.LogException(nameof(GetNextEpisode), ex);
+
                 return ResponseBody<Episode>.Fail("Unexpected error while retrieving the next episode.");
             }
         }
+
         public ResponseBody<List<Episode>> GetWatchedEpisodes(Show show)
         {
             try
             {
-                List<Episode> episodes = show.Seasons
-                    .SelectMany(s => s.Episodes)
+                List<Episode> episodes = GetAllEpisodes(show)
                     .Where(e => e.WhenWatched != null)
+                    .OrderBy(x => x.Number)
                     .ToList();
 
                 return ResponseBody<List<Episode>>.Ok(episodes);
             }
             catch (Exception ex)
             {
-                logger.LogException("GetWatchedEpisodes", ex);
+                logger.LogException(nameof(GetWatchedEpisodes), ex);
+
                 return ResponseBody<List<Episode>>.Fail("Unexpected error while retrieving watched episodes.");
             }
-
         }
+
+        public ResponseBody<List<Episode>> GetUnWatchedEpisodes(Show show)
+        {
+            try
+            {
+                List<Episode> episodes = GetAllEpisodes(show)
+                    .Where(e => e.WhenWatched == null)
+                    .OrderBy(x => x.Number)
+                    .ToList();
+
+                return ResponseBody<List<Episode>>.Ok(episodes);
+            }
+            catch (Exception ex)
+            {
+                logger.LogException(nameof(GetUnWatchedEpisodes), ex);
+
+                return ResponseBody<List<Episode>>.Fail("Unexpected error while retrieving unwatched episodes.");
+            }
+        }
+
         public ResponseBody<double> GetWatchProgress(Show show)
         {
-            GetWatchedEpisodes(Show show);
+            try
+            {
+                ResponseBody<List<Episode>> watched = GetWatchedEpisodes(show);
+                List<Episode> allEpisodes = GetAllEpisodes(show);
+
+                if (!watched.Success || watched.Data == null || allEpisodes.Count == 0)
+                {
+                    return ResponseBody<double>.Fail("Unable to calculate progress: no data.");
+                }
+
+                double percentage = (double)watched.Data.Count / allEpisodes.Count * 100;
+                return ResponseBody<double>.Ok(percentage);
+            }
+            catch (Exception ex)
+            {
+                logger.LogException(nameof(GetWatchProgress), ex);
+                return ResponseBody<double>.Fail("Unexpected error while calculating progress.");
+            }
         }
-        private Episode? FindCurrentEpisode(Show show)
+
+        private static Episode? FindCurrentEpisode(Show show)
         {
-            return show.Seasons
-                .SelectMany(s => s.Episodes)
+            return GetAllEpisodes(show)
                 .FirstOrDefault(e => e.FilePath == show.CurrentEpisodePath);
         }
-        private Episode? GetNextEpisode(Show show, Episode current)
+
+        private static Episode? GetNextEpisode(Show show, Episode current)
         {
-            var allEpisodes = show.Seasons
-                .OrderBy(s => s.Number)
-                .SelectMany(s => s.Episodes.OrderBy(e => e.Number))
-                .ToList();
+            List<Episode> allEpisodes = GetAllEpisodes(show);
 
             int currentIndex = allEpisodes.FindIndex(e => e.FilePath == current.FilePath);
             return (currentIndex + 1 < allEpisodes.Count) ? allEpisodes[currentIndex + 1] : null;
         }
 
+        private static List<Episode> GetAllEpisodes(Show show)
+        {
+            return show.Seasons.OrderBy(s => s.Number).SelectMany(s => s.Episodes.OrderBy(x => x.Number)).ToList();
+        }
     }
 }
