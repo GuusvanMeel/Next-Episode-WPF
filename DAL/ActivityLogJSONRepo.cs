75732c1a1a44a2d1c284fb9f53c4a1f59f722229
GuusvanMeel <vanmeelguus@gmail.com>
2025-06-24 22:52:55 +0200

    added most of activitylog

---
+++ DAL/ActivityLogJSONRepo.cs
@@ -0,0 +1,50 @@
+﻿using Interfaces.Entities;
+using Interfaces.interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    internal class ActivityLogJSONRepo : IActivityLogRepo
+    {
+        private readonly string Logdirectory = Path.Combine(AppContext.BaseDirectory, "data", "shows");
+
+        private readonly ILoggerService logger;
+
+        public ActivityLogJSONRepo(ILoggerService logger)
+        {
+            if (!Directory.Exists(Logdirectory))
+            {
+                Directory.CreateDirectory(Logdirectory);
+            }
+            this.logger = logger;
+        }
+        public ResponseBody DeleteAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ResponseBody<List<ActivityLog>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ResponseBody<List<ActivityLog>> GetForShow(string showTitle)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ResponseBody<List<ActivityLog>> GetRecent(int count)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ResponseBody Log(ActivityLog entry)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Interfaces/Entities/ActivityLog.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces.Entities
+{
+    public class ActivityLog
+    {
+        public DateTime When { get; set; } = DateTime.Now;
+
+        public string? ShowTitle { get; set; } // e.g. "Breaking Bad"
+        public int? Season { get; set; } // Optional
+        public int? Episode { get; set; } // Optional
+        public string? EpisodeTitle { get; set; } // Optional but nice to have
+        public ActivityType Type { get; set; }
+
+        public enum ActivityType
+        {
+            WatchedEpisode,
+            MarkedShowFinished,
+            MarkedShowUnfinished,
+            ResetProgress,
+            DeletedShowMetadata,
+            AddedShow,
+            EditedShow,
+            ChangedRegex,
+            ImportedFolder,
+            StartedApp
+        }
+    }
+}
