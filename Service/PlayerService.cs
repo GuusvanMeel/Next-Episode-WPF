e8bb0fb079429016588390706dee7f7766a3be6d
GuusvanMeel <vanmeelguus@gmail.com>
2025-07-04 23:38:15 +0200

    subtitle bug fixed

--- Service/PlayerService.cs
+++ Service/PlayerService.cs
@@ -37,7 +37,7 @@ namespace Service
                     StartInfo = new ProcessStartInfo
                     {
                         FileName = playerPath,
-                        Arguments = $"--fullscreen --sub-track=1 \"{videofilepath}\"",
+                        Arguments = $"--fullscreen \"{videofilepath}\"",
                         UseShellExecute = false,
                         CreateNoWindow = true,
                     }
